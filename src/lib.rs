a557c9d02d37101603e2fad8b0c4c502b7de80b8
m. interrupt <misterinterrupt@gmail.com>
2023-02-25 17:01:44 -0800

    static lib w/ cbindgen .h

--- src/lib.rs
+++ src/lib.rs
@@ -3,12 +3,14 @@
 //! You have to short the TX and RX pins to make this program work
 
 #![allow(clippy::empty_loop)]
-#![deny(unsafe_code)]
 #![no_main]
 #![no_std]
 
-use core::default::Default;
+extern crate alloc;
 
+use core::{default::Default, u8};
+
+use alloc::{boxed::Box, vec::{self, Vec}};
 use panic_halt as _;
 
 use nb::block;
@@ -16,21 +18,26 @@ use nb::block;
 use cortex_m_rt::entry;
 use stm32f1xx_hal::{
     pac,
+    gpio,
     prelude::*,
     serial::{Config, Serial},
+    serial::*
 };
 use unwrap_infallible::UnwrapInfallible;
 
-// struct my{}
-// trait mt_t{
-//     fn funco();
-// }
-// impl mt_t for my {
-//     fn my_t
-// }
+#[no_mangle]
+pub static CR: u8 = b'\r';
+#[no_mangle]
+pub static LF: u8 = b'\n';
 
-#[entry]
-fn main() -> ! {
+pub struct SerialInterfaceContext {
+    rx: Rx<pac::USART2>,
+    tx: Tx<pac::USART2>,
+    chars: Vec<u8>,
+}
+
+#[no_mangle]
+pub unsafe extern "C" fn rust_serial_interface_new() -> *mut SerialInterfaceContext {
     // Get access to the device specific peripherals from the peripheral access crate
     let p = pac::Peripherals::take().unwrap();
 
@@ -66,65 +73,37 @@ fn main() -> ! {
     // let tx = gpiob.pb10.into_alternate_push_pull(&mut gpiob.crh);
     // // Take ownp.USART2usart device. Take ownership over the USART register and tx/rx pins. The rest of
     // the registers are used to enable and configure the device.
-    let mut serial = Serial::new(
+    // let mut SERIAL:Serial<stm32f1xx_hal::pac::USART2, (stm32f1xx_hal::gpio::Pin<'A', 2, stm32f1xx_hal::gpio::Alternate>, stm32f1xx_hal::gpio::Pin<'A', 3>)> = Serial::new(
+    let serial = Serial::new(
         p.USART2,
         (tx, rx),
         &mut afio.mapr,
         Config::default().baudrate(9600.bps()),
         &clocks,
     );
+    let context = SerialInterfaceContext {
+        rx: serial.rx,
+        tx: serial.tx,
+        chars: Vec::new(),
+    };
+    Box::into_raw(Box::new(context))
+}
 
-    let send = b'>'; // let sent = b'X';
-    let cr = b'\r';
-    let lf = b'\n';
+#[no_mangle]
+pub unsafe extern "C" fn rust_serial_read(serial_ptr: *mut SerialInterfaceContext) -> u8 {
+    
+    let mut serial_context = Box::from_raw(serial_ptr);
+    let rx = &mut serial_context.rx;
+    // Read the byte that was just sent. Blocks until the read is complete
+    block!(rx.read()).unwrap()
+}
 
-    let mut buffer: [u8; 32] = Default::default();
-    let mut done = false;
-    let mut pos = 0;
-
-    loop {
-        // Read the byte that was just sent. Blocks until the read is complete
-
-        // serial.rx.read()
-        let received = block!(serial.rx.read()).unwrap();
-        buffer[pos] = received;
-        if buffer[pos] == b'\r' {
-            done = true;
-        }
-        pos += 1;
-
-
-        if done {
-            block!(serial.tx.write(send)).unwrap_infallible();
-            while pos > 0 {
-                block!(serial.tx.write(buffer[pos - 1])).unwrap_infallible();
-                pos -= 1
-            }
-            block!(serial.tx.write(lf)).unwrap_infallible();
-            block!(serial.tx.write(cr)).unwrap_infallible();
-            done = false
-        }
-
-    }
-
-    // // Loopback test. Write `X` and wait until the write is successful.
-   
-
-    // let sent = b'X';
-    // block!(serial.tx.write(sent)).unwrap_infallible();
-    // // Since we have connected tx and rx, the byte we sent should be the one we received
-    // assert_eq!(received, sent);
-
-    // // Trigger a breakpoint to allow us to inspect the values
-    // asm::bkpt();
-
-    // // You can also split the serial struct into a receiving and a transmitting part
-    // let (mut tx, mut rx) = serial.split();
-    // let sent = b'Y';
-    // block!(tx.write(sent)).unwrap_infallible();
-    // let received = block!(rx.read()).unwrap();
-    // assert_eq!(received, sent);
-    // asm::bkpt();
-
-    // loop {}
+#[no_mangle]
+pub unsafe extern "C" fn rust_serial_write(serial_ptr: *mut SerialInterfaceContext, value: u8) {
+    let mut serial_context = Box::from_raw(serial_ptr);
+    let tx = &mut serial_context.tx;
+        block!(tx.write(value)).unwrap_infallible();
+        block!(tx.write(LF)).unwrap_infallible();
+        block!(tx.write(CR)).unwrap_infallible();
+    return 0;
 }
